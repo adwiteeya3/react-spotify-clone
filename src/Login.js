f5727f17f6dde198ab0f0b42d106064a720c3696
Adwiteeya <adwiteeya3@gmail.com>
2020-08-06 11:05:05 +0530

    update

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Login.css';
+import { loginUrl } from './spotify';
 
 function Login() {
     return (
@@ -11,7 +12,7 @@ function Login() {
             />
 
     {/* login with spotify button */}
-            <a> LOGIN WITH SPOTIFY</a>
+            <a href={loginUrl}> LOGIN WITH SPOTIFY</a>
         </div>
     )
 }
--- src/spotify.js
+++ src/spotify.js
@@ -2,18 +2,19 @@
 
 // where we actually send users to login to spotify
 export const authEndpoint= "https://accounts.spotify.com/authorize";
+//export const authEndpoint= "https://accounts.spotify.com/en/login/";
 
 // after login, the below line redirects to spotify home page
-const redirectUri= "http://localhost:3000/"
-const clientId= "5db405b7ba2945a19af1b7d78ff9fac8"
+const redirectUri = "http://localhost:3000/";
+const clientId= "5a8366f1269a4237bb5fc0bd3e077f55";
 
-const scope= [
+const scopes = [
     "user-read-currently-playing",
-    "user-read-recently-playing",
+    "user-read-recently-played",
     "user-read-playback-state",
     "user-top-read",
     "user-modify-playback-state",
-];
+  ];
 
-export const loginUrl= `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token`;
+export const loginUrl= `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
 
