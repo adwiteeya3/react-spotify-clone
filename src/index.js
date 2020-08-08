abeb899ee9943f8a4dbabc124defd329c03a406f
Adwiteeya <adwiteeya3@gmail.com>
2020-08-08 23:55:49 +0530

    update

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import { DataLayer } from './DataLayer';
 import * as serviceWorker from './serviceWorker';
-import { initialState } from './reducer';
+import reducer, { initialState } from './reducer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <DataLayer initialState={initialState} reducer> 
+    <DataLayer initialState={initialState} reducer={reducer}> 
       <App />
     </DataLayer>
     
--- src/reducer.js
+++ src/reducer.js
@@ -7,8 +7,16 @@ export const initialState = {
 
 const reducer = (state, action) => {
     console.log(action);
+
+    // Action -> type, [payloag]
     switch(action.type) {
-        
+        case 'SET_USER':
+            return {
+                ...state,
+                user: action.user
+            }
+    default:
+        return state;
     }
 }
 
