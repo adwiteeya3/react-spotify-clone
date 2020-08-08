70045c9b9f1527cbaf9a161f4ab78992aef12a86
Adwiteeya <adwiteeya3@gmail.com>
2020-08-08 21:52:34 +0530

    split authentication

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+  //run code based on a given condition
+  useEffect(() => {
+    const token= getTokenFromUrl();
+    console.log("I have a token: ", token);
+  }, []);
+  
   return (
+      //BEM
     <div className="app">
       {/* Spotify logo */}
       {/* login with spotify button */}
