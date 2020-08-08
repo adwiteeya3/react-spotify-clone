f820ee73f978c250ac941879ba1d01fccfef9b89
Adwiteeya <adwiteeya3@gmail.com>
2020-08-08 22:06:00 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   //run code based on a given condition
@@ -10,28 +13,20 @@ function App() {
   useEffect(() => {
     const hash= getTokenFromUrl();
     window.location.hash = "";
-    const token = hash.access_token;
+    const _token = hash.access_token;
 
-    if (token) {
-      setToken(token)
+    if (_token) {
+      setToken(_token)
     }
     
     console.log("I have a token: ", token);
   }, []);
   
   return (
-      //BEM
     <div className="app">
       {
-        token ? (
-          <h1>I am logged in</h1>
-        ) : (
-          <Login />
-        )
+        token ? (<h1>I am logged in</h1>) : (<Login />)
       }
-      {/* Spotify logo */}
-      {/* login with spotify button */}
-      <Login />
 
     </div>
   );
