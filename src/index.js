def0a86a3442a5d0b9dac1a9a3bf1f3d427a2f00
Adwiteeya <adwiteeya3@gmail.com>
2020-08-08 23:47:34 +0530

    DataLayer

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { DataLayer } from './DataLayer';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <DataLayer initialState reducer> 
+      <App />
+    </DataLayer>
+    
+  </React.StrictMode >,
   document.getElementById('root')
 );
 
