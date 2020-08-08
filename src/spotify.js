a478993423bbe606a0b141ce0fa8a6c58da3a16c
Adwiteeya <adwiteeya3@gmail.com>
2020-08-08 21:44:43 +0530

    update

--- src/spotify.js
+++ src/spotify.js
@@ -16,5 +16,17 @@ const scopes = [
     "user-modify-playback-state",
   ];
 
+  export const getTokenFromUrl= () => {
+    return window.location.hash
+      .substring(1)
+      .split('&')
+      .reduce((initial, item) => {
+        let parts= item.split('=');
+        initial[parts[0]] = decodeURIComponent(parts[1])
+
+        return initial;
+      }, {});
+  }
+
 export const loginUrl= `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
 
