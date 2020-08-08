ce1010b436ce0bdce6e58f8afc9c13a9886da5e9
Adwiteeya <adwiteeya3@gmail.com>
2020-08-08 21:58:52 +0530

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
 
 function App() {
   //run code based on a given condition
+  const [token, setToken]= useState(null);
+
   useEffect(() => {
-    const token= getTokenFromUrl();
+    const hash= getTokenFromUrl();
+    window.location.hash = "";
+    const token = hash.access_token;
+
+    if (token) {
+      setToken(token)
+    }
+    
     console.log("I have a token: ", token);
   }, []);
   
   return (
       //BEM
     <div className="app">
+      {
+        token ? (
+          <h1>I am logged in</h1>
+        ) : (
+          <Login />
+        )
+      }
       {/* Spotify logo */}
       {/* login with spotify button */}
       <Login />
