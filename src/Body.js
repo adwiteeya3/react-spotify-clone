d2d320423df227dd1c84b6e904bb02ab40161051
Adwiteeya <adwiteeya3@gmail.com>
2020-08-09 22:38:48 +0530

    body

--- src/Body.js
+++ src/Body.js
@@ -6,6 +6,15 @@ function Body({ spotify }) {
     return (
         <div className='body'>
             <Header spotify={spotify}/>
+
+            <div className='body_info'>
+                <img src='\image\spotify.jpg' alt='' />
+                <div className='body_infoText'>
+                    <strong>PLAYLIST</strong>
+                    <h2>Discover Weekly</h2>
+                    <p> Discription..</p>
+                </div>
+            </div>
         </div>
     );
 }
