5f67541ab3e8c10cd37d58ec3eda1850e4a477a8
Adwiteeya <adwiteeya3@gmail.com>
2020-08-09 14:22:22 +0530

    files

--- src/Player.js
+++ src/Player.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import "./Player.css";
+import Sidebar from './Sidebar';
+import Body from './Body';
 
 function Player({ spotify }) {
     return (
         <div className="player">
             <div className="player_body"> 
+            
             { /* Sidebar */}
+            <Sidebar />
+            
             { /* Body */}
+            <Body />
+            
             </div>
             
             { /* Footer */}
