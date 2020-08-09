3c238fe2b5a5b4804eaca8f5af15956fa7c81c54
Adwiteeya <adwiteeya3@gmail.com>
2020-08-09 22:09:16 +0530

    body header done

--- src/Header.js
+++ src/Header.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import { Avatar } from '@material-ui/core';
+import { useDataLayerValue} from './DataLayer';
 
 function Header() {
-    const [{ user }, dispatch ] = useStateValue();
+    const [{ user }, dispatch ] = useDataLayerValue();
 
     return (
         <div className='header'>
@@ -17,8 +18,8 @@ function Header() {
             </div>
 
             <div className='header_right'>
-                <Avatar src='' alt='AD' />
-                <h4>Adwiteeya</h4>
+                <Avatar src={user?.images[0]?.url} alt='AD' />
+                <h4>{user?.display_name}</h4>
 
             </div>
 
