352fcfa60c703ebec2b30beb642add10ece9b97f
Adwiteeya <adwiteeya3@gmail.com>
2020-08-09 16:55:26 +0530

    sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,7 +4,6 @@ import SidebarOption from './SidebarOption';
 import HomeIcon from '@material-ui/icons/Home';
 import SearchIcon from '@material-ui/icons/Search';
 import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
-import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
 
 
 function Sidebar() {
@@ -14,9 +13,9 @@ function Sidebar() {
              src='https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg' 
              alt='' 
             />
-            <SidebarOption title="Home" />
-            <SidebarOption title="Search" />
-            <SidebarOption title="Your Library" />
+            <SidebarOption Icon={HomeIcon} title="Home" />
+            <SidebarOption Icon={SearchIcon} title="Search" />
+            <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
 
         </div>
     );
--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -4,9 +4,10 @@ import './SidebarOption.css';
 function SidebarOption({ title, Icon }) {
     return (
         <div className='sidebarOption'>
-            <p> {title} </p>
+            {Icon && <Icon classname='sidebarOption_icon' />}
+            {Icon ? <h4> {title} </h4> : <p> {title} </p>}
         </div>
-    )
+    );
 }
 
 export default SidebarOption;
