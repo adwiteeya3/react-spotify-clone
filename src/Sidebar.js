56ee996996c314ed43dbfde70004504e19d7b382
Adwiteeya <adwiteeya3@gmail.com>
2020-08-09 14:25:29 +0530

    display player

---
+++ src/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sidebar() {
+    return (
+        <div className='sidebar'>
+            <h1> I am a sidebar </h1>
+        </div>
+    )
+}
+
+export default Sidebar
